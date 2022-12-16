7331eea2fe6733bdcd386fa504f6d0f15a67b856
charliesabino <94320712+charliesabino@users.noreply.github.com>
2022-12-15 21:46:24 -0500

    day 5

---
+++ p5/sol.py
@@ -0,0 +1,52 @@
+import re
+
+
+def p1():
+    s = []
+
+    x = open("input.txt")
+
+    for line in x:
+        if line == "\n":
+            break
+        s.append(
+            [line[k * 4 + 1] for k in range(len(line) // 4)]
+        )  # this is just line[1::4] - thanks UnrelatedString
+
+    s.pop()
+    s = [list("".join(c).strip()[::-1]) for c in zip(*s)]
+
+    for line in x:
+        a, b, c = map(int, re.findall("\\d+", line))
+        s[c - 1].extend(s[b - 1][-a:][::-1])
+        s[b - 1] = s[b - 1][:-a]
+
+    print("".join([a[-1] for a in s]))
+
+
+def p2():
+
+    s = []
+
+    x = open("input.txt")
+
+    for line in x:
+        if line == "\n":
+            break
+        s.append(
+            [line[k * 4 + 1] for k in range(len(line) // 4)]
+        )  # this is just line[1::4] - thanks UnrelatedString
+
+    s.pop()
+    s = [list("".join(c).strip()[::-1]) for c in zip(*s)]
+
+    for line in x:
+        a, b, c = map(int, re.findall("\\d+", line))
+        s[c - 1].extend(s[b - 1][-a:])
+        s[b - 1] = s[b - 1][:-a]
+
+    print("".join([a[-1] for a in s]))
+
+
+p1()
+p2()
